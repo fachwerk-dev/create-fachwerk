f11cbefcada488ff8cf92ae4ff0da0cbb109918a
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-05-23 09:16:44 +0300

    feat: better node examples

--- node/example2.mjs
+++ node/example2.mjs
@@ -1,16 +1,15 @@
-import { promises } from "fs";
+import { writeFile } from "fs/promises";
+import { Fachwerk } from "fachwerk";
+import { Resvg } from "@resvg/resvg-js";
+
 import { createSSRApp } from "vue";
 import { renderToString } from "vue/server-renderer";
-import { Resvg } from "@resvg/resvg-js";
-import { Fachwerk } from "fachwerk";
 
-const template = `
+const svg = `
 <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
   <path
-    :d="circlepoints(24,50)
-      .map(point => circlepath(50,point))
-      .join('')
-    "
+    v-for="point in circlepoints(24,50)"
+    :d="circlepath(50,point)"
     fill="none"
     stroke="lightblue"
     stroke-width="2"
@@ -20,10 +19,10 @@ const template = `
 `;
 
 const app = createSSRApp({
-  template,
+  template: svg,
 });
 app.use(Fachwerk);
 
 const renderedSvg = await renderToString(app);
 const png = new Resvg(renderedSvg).render().asPng();
-await promises.writeFile("./example.png", png);
+await writeFile("./example2.png", png);
