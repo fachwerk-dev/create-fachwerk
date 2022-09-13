bfb069bedae0d408a34645860a6fbbdcfdb3785d
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-09-13 23:17:11 +0300

    feat: figma

--- figma/code.js
+++ figma/code.js
@@ -1,19 +1,40 @@
 figma.showUI(__html__);
 figma.ui.hide();
-figma.ui.onmessage = ({ message, points }) => {
-  if (message === "create") {
+figma.ui.onmessage = ({ message, circles }) => {
+  if (message === "circles") {
+    // Set up a frame
+    const frame = figma.createFrame();
+    frame.name = "Figma and Fachwerk";
+    frame.x = -150;
+    frame.y = -150;
+    frame.resize(500, 500);
+
     const nodes = [];
-    points.forEach(([x, y]) => {
+
+    // Loop over the received circle coordinates
+    // and create Figma circles
+
+    circles.forEach(([x, y], i) => {
       const circle = figma.createEllipse();
+      circle.name = `Ellipse ${i + 1}`;
       circle.x = x;
       circle.y = y;
-      circle.resize(100, 100);
-      circle.fills = [{ type: "SOLID", color: { r: 1, g: 0, b: 0 } }];
+      circle.resize(200, 200);
+      circle.strokeWeight = 2;
+      circle.strokes = [
+        { type: "SOLID", color: { r: 173 / 255, g: 216 / 255, b: 230 / 255 } },
+      ];
+      circle.fills = [];
       figma.currentPage.appendChild(circle);
       nodes.push(circle);
     });
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+
+    // Group and zoom in on the circles
+
+    const group = figma.group(nodes, frame);
+    group.expanded = false;
+    figma.currentPage.selection = [group];
+    figma.viewport.scrollAndZoomIntoView([group]);
   }
   figma.closePlugin();
 };
